40237c5473d643758ca5a25bfee2be26d6199ec0
DingJS <1554308380@qq.com>
2018-12-27 16:08:01 +0800

    订单连表查询订单项与订单商品信息 .java与mapper文件更改

---
+++ demo/src/main/java/com/example/demo/model/ProductOrderItem.java
@@ -0,0 +1,64 @@
+package com.example.demo.model;
+
+public class ProductOrderItem {
+    private Integer productId;
+
+    private Integer categoryId;
+
+    private String productName;
+
+    private Double price;
+
+    private String image;
+
+    private Integer status;
+
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
